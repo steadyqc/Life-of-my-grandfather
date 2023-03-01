526e5a6532cba5c073c8d1d0376e33569d0510df
Kevin <steadfast_emcee@hotmail.com>
2023-03-01 17:42:37 -0500

    ready for PR

--- scripts/fontsize.js
+++ scripts/fontsize.js
@@ -1,13 +1,16 @@
 // Define the maximum and minimum font sizes for p, h2, and h3 elements
 var pMaxSize = 25;
+var figcaptionMaxSize = 25;
 var h2MaxSize = 30;
 var h3MaxSize = 28;
 var pMinSize = 13;
+var figcaptionMinSize = 13;
 var h2MinSize = 16;
 var h3MinSize = 15;
 
 function increaseFontSize() {
   var p = document.getElementsByTagName('p');
+  var figcaption = document.getElementsByTagName('figcaption');
   var h2 = document.getElementsByTagName('h2');
   var h3 = document.getElementsByTagName('h3');
   for (i = 0; i < p.length; i++) {
@@ -16,6 +19,13 @@ function increaseFontSize() {
       s += 1;
     }
     p[i].style.fontSize = s + "px";
+  }
+  for (i = 0; i < figcaption.length; i++) {
+    var s = parseInt(window.getComputedStyle(figcaption[i]).fontSize.replace("px", ""));
+    if (s < pMaxSize) {
+      s += 1;
+    }
+    figcaption[i].style.fontSize = s + "px";
   }
   for (i = 0; i < h2.length; i++) {
     var s = parseInt(window.getComputedStyle(h2[i]).fontSize.replace("px", ""));
@@ -35,8 +45,23 @@ function increaseFontSize() {
 
 function decreaseFontSize() {
   var p = document.getElementsByTagName('p');
+  var figcaption = document.getElementsByTagName('figcaption');
   var h2 = document.getElementsByTagName('h2');
   var h3 = document.getElementsByTagName('h3');
+  for (i = 0; i < p.length; i++) {
+    var s = parseInt(window.getComputedStyle(p[i]).fontSize.replace("px", ""));
+    if (s > pMinSize) {
+      s -= 1;
+    }
+    p[i].style.fontSize = s + "px";
+  }
+  for (i = 0; i < figcaption.length; i++) {
+    var s = parseInt(window.getComputedStyle(figcaption[i]).fontSize.replace("px", ""));
+    if (s > pMinSize) {
+      s -= 1;
+    }
+    figcaption[i].style.fontSize = s + "px";
+  }
   for (i = 0; i < p.length; i++) {
     var s = parseInt(window.getComputedStyle(p[i]).fontSize.replace("px", ""));
     if (s > pMinSize) {
